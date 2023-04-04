492edcdcfc30d8a27c41444cfd0d12a316c6308b
Keyv Chan <keyvchan@gmail.com>
2023-04-04 19:04:08 +0800

    fix: disable sematic highlighting for now

--- lua/monokai/init.lua
+++ lua/monokai/init.lua
@@ -4,4 +4,9 @@ vim.cmd("syntax reset")
 vim.g.colors_name = "monokai"
 vim.opt.termguicolors = true
 
+-- disable semantic highlighting
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+	vim.api.nvim_set_hl(0, group, {})
+end
+
 require("monokai.theme")
